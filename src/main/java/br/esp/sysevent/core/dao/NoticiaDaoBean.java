003c885291d413d1be0ae1f3e3efb62dfbbcb6c1
Alexander <Alexander@192.168.25.192>
2015-07-26 14:22:15 -0300

    Migração para mocca

--- src/main/java/br/esp/sysevent/core/dao/NoticiaDaoBean.java
+++ src/main/java/br/esp/sysevent/core/dao/NoticiaDaoBean.java
@@ -5,6 +5,9 @@
 package br.esp.sysevent.core.dao;
 
 import br.esp.sysevent.core.model.Noticia;
+import com.javaleks.commons.core.dao.annotation.PreSave;
+import com.javaleks.commons.core.dao.annotation.PreSaveOrUpdate;
+import com.javaleks.commons.util.CalendarUtils;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -21,12 +24,12 @@ public class NoticiaDaoBean extends AbstractBaseSistemaDaoBean<Long, Noticia> im
         super(sessionFactory);
     }
 
-//    @PreSave
-//    @PreSaveOrUpdate
-//    protected void checkData(final Noticia entity) {
-//        // set the date if necessary
-//        if (entity.getData() == null) {
-//            entity.setData(CalendarUtils.today());
-//        }
-//    }
+    @PreSave
+    @PreSaveOrUpdate
+    protected void checkData(final Noticia entity) {
+        // set the date if necessary
+        if (entity.getData() == null) {
+            entity.setData(CalendarUtils.today());
+        }
+    }
 }
